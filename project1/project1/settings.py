366c2aefef78f092bcf04585bed6d02c7047494e
Mohit-368 <berwaliamohit@gmail.com>
2026-02-24 22:34:50 +0530

    learning about urls

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_URL='/accounts/login'
+LOGIN_URL='/login'
 
 LOGIN_REDIRECT_URL='/post/list/'
 LOGOUT_REDIRECT_URL='/'
